0412b577f22f1a5d2cd2e0f2b981068bdeec4c21
Álvaro Díaz Fuentes <a.diaz@cesjuanpablosegundocadiz.es>
2021-05-05 17:23:35 +0200

    version de rama nueva

---
+++ Proyecto Visual Studio con Git y GitHub/FormRamaNueva.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Visual_Studio_con_Git_y_GitHub
+{
+    public partial class FormRamaNueva : Form
+    {
+        public FormRamaNueva()
+        {
+            InitializeComponent();
+        }
+    }
+}
