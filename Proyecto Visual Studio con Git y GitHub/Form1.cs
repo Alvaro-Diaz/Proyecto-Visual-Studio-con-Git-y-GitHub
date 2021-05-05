1e4b84d4c994691820757bbe5d2bae76fcc813b4
Álvaro Díaz Fuentes <a.diaz@cesjuanpablosegundocadiz.es>
2021-05-05 17:07:01 +0200

    Agregar archivos de proyecto.

---
+++ Proyecto Visual Studio con Git y GitHub/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Visual_Studio_con_Git_y_GitHub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
